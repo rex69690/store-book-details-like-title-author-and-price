be101d749932895bec842baca36b573c5626db3e
Tushar Koundal <154369254+rex69690@users.noreply.github.com>
2024-04-24 13:31:30 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+struct book
+{
+char title [20];
+char author [20];
+int price ;
+}s[3];
+int main()
+{
+  int i;
+  for(i=0;i<3;i++)
+    {
+    printf("enter book input: \n ");
+    gets(s[i].title);
+    gets(s[i].author);
+    scanf("%d",&s[i].price);
+    getchar();
+    }
+  for(i=0;i<3;i++)
+  {
+    printf("Title: %s\n Author: %s\n Price: %d\n",s[i].title,s[i].author,s[i].price);
+  }
+  return 0;
+}
